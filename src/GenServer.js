7d3d13019ba187345279744e2be95d383fca5050
Maz Baig <mazbaig@Mazs-MacBook-Air.local>
2021-05-25 21:06:04 -0500

    Wrote down some code as a model. Will likely change completely after whiteboarding

---
+++ src/GenServer.js
@@ -0,0 +1,56 @@
+'use strict'
+
+export default class GenServer {
+
+    workerMap = {};
+
+    constructor(genServerName) {
+        this.genServerName = genServerName;
+    }
+
+    /**
+     * @param {string} processName Unique name for the process you wish to spawn
+     * @param {Function} process stateless function that you wish to run in the server
+     * @param {object} initialState Initial state for the process you wish to spawn
+     */
+    startLink( processName, process, initialState) {
+
+        const blob = new Blob([`(
+            while(true) {
+                ${process.toString()}
+            }
+        )();`], { type: 'application/javascript' })
+
+        if(!!this.workerMap[processName]) {
+            console.warn(`${processName} already exists in the genserver named '${this.genServerName}'`);
+        }
+        
+        const worker = new Worker(window.URL.createObjectURL(blob));
+
+        this.workerMap[processName] = worker;
+
+        return worker;
+
+    }
+
+    /**
+     * 
+     * @param {Function} process stateless function that you wish to run in the server
+     */
+    killProcess(processName) {
+        worker = this.workerMap[processName];
+
+        if(!!worker) {
+            worker.terminate();
+        }
+    }
+
+    killAllProcesses() {
+
+    }
+
+    killServer() {
+
+    }
+
+}
---
+++ src/Servable.js
@@ -0,0 +1,7 @@
+export default class Servable {
+    handleCall(event, caller, current_state) {
+
+        console.error("not implemented");
+
+    }
+}

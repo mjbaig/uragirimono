d8b60a6674cb0c16fd0864b455db8c756f8119a2
Maz Baig <mazbaig@Mazs-MacBook-Air.local>
2021-06-08 18:09:48 -0500

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Uragirimono
-I'm creating a communication framework for something I need in a different project. If you want to use it, go ahead.
+I'm creating a communication library for something I need in a different emulation project. If you want to use it, go ahead.
 
-I'm modeling this after Elixir's GenServers, but I'll probably stray from that model if I feel that its necessary for what I'm trying to do.
+Yes, what I'm doing is very dumb, but that's why I did it.
--- test/Uragirimono.test.ts
+++ test/Uragirimono.test.ts
@@ -1,9 +1,39 @@
 import { expect } from 'chai';
 import {uragirimono} from '../src/Uragirimono';
 
-describe('Options tests', () => { // the tests container
-    it('checking default options', () => { // the single test
+var value = 0;
+
+describe('Uragirimono tests', () => { // the tests container
+    it('create new channel', () => { // the single test
         /* fps limit */
-        expect(30).to.equal(30); // As I said 3 lines above
+        uragirimono.registerChannel("testChannel");
+        
+
+        expect(uragirimono.channels.get("testChannel")).to.not.equal(undefined);
+    });
+
+    it('add subscriber', () => {
+
+        uragirimono.registerSubscriber("testChannel", {
+            update() {
+                console.log("klasjdf");
+                value = 1;
+            }
+        })
+    });
+
+    it('send message', () => { // the single test
+        /* fps limit */
+        uragirimono.send({
+            channelName: "testChannel",
+            payload: {test: 0}
+        });
+
+        expect(value).to.equal(1);
+    });
+
+    it('create destroy channel', () => { // the single test
+        /* fps limit */
+        uragirimono.destroyChannel("testChannel");
     });
 });
